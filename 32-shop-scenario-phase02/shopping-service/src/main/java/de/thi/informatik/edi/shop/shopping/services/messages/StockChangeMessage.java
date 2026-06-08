4e964aa83b55ab6fe0ec5d1c86d1a9bccbf037ef
Marloto <marloto@codestack.de>
2026-06-08 08:54:11 +0200

    small fix in scenarios

--- 32-shop-scenario-phase02/shopping-service/src/main/java/de/thi/informatik/edi/shop/shopping/services/messages/StockChangeMessage.java
+++ 32-shop-scenario-phase02/shopping-service/src/main/java/de/thi/informatik/edi/shop/shopping/services/messages/StockChangeMessage.java
@@ -1,11 +1,18 @@
 package de.thi.informatik.edi.shop.shopping.services.messages;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class StockChangeMessage {
 	private double value;
-	
+
 	public StockChangeMessage() {
 	}
-	
+
+	@JsonCreator
+	public StockChangeMessage(double value) {
+		this.value = value;
+	}
+
 	public double getValue() {
 		return value;
 	}
